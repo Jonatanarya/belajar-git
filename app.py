bda1028016049e94d044c4f89afa392081ece99c
Jonatan Arya Santosa <2313020222@mhs.univ.ac.id>
2026-06-10 22:26:25 +0700

    [FEAT] Implementasi environment variables pada Dockerfile

--- app.py
+++ app.py
@@ -1,28 +1,8 @@
-from views.dashboard_component import render_dashboard, fetch_data_from_api
-from controllers.api_handler import get_users
-
-app_state = {"items": [], "is_loading": True}
-
-
-def update_state(new_data):
-    app_state["items"] = new_data
-    app_state["is_loading"] = False
-
-
+import os
+# Membaca variabel 'APP_USER', defaultnya adalah 'Guest'
+user_name = os.getenv('APP_USER', 'Guest')
+# Membaca variabel 'APP_ENV', defaultnya adalah 'development'
+app_env = os.getenv('APP_ENV', 'development')
 if __name__ == "__main__":
-    # Proses Integrasi
-    data = fetch_data_from_api(get_users)
-    if data:
-        render_dashboard(data)
-
-    print("Kondisi data sedang loading:")
-    render_dashboard(app_state["items"], app_state["is_loading"])
-
-    print()
-    print("Kondisi data sudah tampil:")
-    mock_data = [
-        {"id": 101, "name": "Produk A"},
-        {"id": 102, "name": "Produk B"},
-    ]
-    update_state(mock_data)
-    render_dashboard(app_state["items"], app_state["is_loading"])
+    print(f"Halo {user_name}! Aplikasi ini berjalan di dalam kontainer Docker.")
+    print(f"Lingkungan: {app_env}")
